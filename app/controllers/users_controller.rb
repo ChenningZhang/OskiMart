0d38020c0b38373aa1815377573ad3704abec3b7
Chenning Zhang <czhang1306@berkeley.edu>
2015-10-20 16:51:56 -0700

    Show error messages for invalid Signup, empty name, email, etc.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,10 +8,15 @@ class UsersController < ApplicationController
   end
 
   def create
-  	@user = User.new(params[:user])
+  	@user = User.new(user_params)
   	if @user.save
   	else
   		render 'new'
   	end
   end
+
+  private
+  	def user_params
+  		params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  	end
 end
