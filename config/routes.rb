e093535ca7de3ff2ea82693f3aa0ae222256a241
Chenning Zhang <czhang1306@berkeley.edu>
2015-10-20 16:10:20 -0700

    Sign up page is ready.

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ Rails.application.routes.draw do
 
   get 'users/new'
 
+  resources :users
+  resources :posts
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
