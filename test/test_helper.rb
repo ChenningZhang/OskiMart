fcee5178fbea0eb6737ea0210ac2bf9b213fd978
Chenning Zhang <czhang1306@berkeley.edu>
2015-10-23 13:41:41 -0700

    testing setup

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,8 @@ SimpleCov.start 'rails'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'minitest/reporters'
+Minitest::Reporters.use!
 
 
 class ActiveSupport::TestCase
