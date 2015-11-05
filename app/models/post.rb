a690003e72236ccc61ed1e88050394b546f7ad42
Lucas Zhang <lucas.zhang15@gmail.com>
2015-11-05 11:39:43 -0800

    Basic search working

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,8 +10,11 @@ class Post < ActiveRecord::Base
     puts "model self.search called"
     puts(search)
     puts(search_category)
-    where("title LIKE ?", "%#{search}%")
-    where("description LIKE ?", "%#{search}%")
-    where("category LIKE ?", "%#{search_category}%")
+    keyword_regex = "%#{search}%"
+    category_regex = "%#{search_category}%"
+    where("(title LIKE ? or description LIKE ?) and category LIKE ?", keyword_regex, keyword_regex, category_regex)
+    #where("title LIKE ?", "%#{search}%")
+    #where("description LIKE ?", "%#{search}%")
+    #where("category LIKE ?", "%#{search_category}%")
   end
 end
