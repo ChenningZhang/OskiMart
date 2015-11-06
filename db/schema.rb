0ba4434e4a4774d0470bb9c1cf993f1f82c69f9d
Chenning Zhang <czhang1306@berkeley.edu>
2015-11-06 14:22:36 -0800

    Updated users login & signup tests. Added post model tests.

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20151105205325) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -72,7 +73,7 @@ ActiveRecord::Schema.define(version: 20151105205325) do
     t.string   "title"
     t.string   "description"
     t.string   "category"
-    t.integer  "price"
+    t.string   "price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
