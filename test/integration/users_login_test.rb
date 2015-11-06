cf866b2ec9b2c3956f760f688b37fa353e270c8d
Chenning Zhang <czhang1306@berkeley.edu>
2015-11-05 20:42:46 -0800

    Added login and signup tests. (In progress)

---
+++ test/integration/users_login_test.rb
@@ -0,0 +1,6 @@
+require 'test_helper'
+
+class UsersLoginTest < ActionDispatch::IntegrationTest
+	# get login_path, assert_template, do login and assert_template
+	#setup method signup a user, teardown method destroys it
+end
