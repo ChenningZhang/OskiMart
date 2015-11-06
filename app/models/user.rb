bd987c73701eeef2741268e73c9bfa4a093f6da6
Chenning Zhang <czhang1306@berkeley.edu>
2015-11-05 18:09:21 -0800

    Added email validation to allow Berkeley emails only.

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,10 +12,8 @@ class User < ActiveRecord::Base
 
 	#Valid sign up fields presence as well as email format.
 	before_save { self.email = email.downcase }
-	#validates :first_name, presence: true
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :name, presence: true
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@berkeley+\.edu+\z/i
 	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
-	#validates :username, presence: true
 	validates :password, presence: true
-	#has_secure_password
 end
