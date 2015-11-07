74d50bb4b41f31bd96893e84597c313ad8640f33
Lucas Zhang <lucas.zhang15@gmail.com>
2015-11-06 17:40:18 -0800

    Added search tests to post model tests

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -7,6 +7,7 @@ class PostTest < ActiveSupport::TestCase
 	end
 
 	test "should be valid" do
+		#puts Post.all.inspect
 		assert @post.valid?
 	end
 
@@ -29,4 +30,28 @@ class PostTest < ActiveSupport::TestCase
 		@post.price = "  "
 		assert_not @post.valid?
 	end
+
+
+	test "search by keyword" do
+		keyword1 = 'Refrigerator'
+		keyword2 = 'refrigerator'
+		keyword3 = 'warm'
+		keyword4 = 'clAssic'
+		keyword5 = 'book'
+
+		search_posts = Post.search(keyword1)
+		assert_equal(posts(:refrigerator), search_posts.first, 'Exact search for Refrigerator returns wrong post')
+		search_posts = Post.search(keyword2)
+		assert_equal(posts(:refrigerator), search_posts.first, 'Case insensitive search for refrigerator returns wrong post')
+		search_posts = Post.search(keyword3)
+		assert_equal(posts(:jacket), search_posts.first, 'Search for warm does not return jacket post')
+		search_posts = Post.search(keyword4)
+		assert_equal(posts(:harry_potter), search_posts.first, 'Case insensitive search for clAssic does not return harry potter book')
+
+
+		search_posts = Post.search(keyword5) #should return 2 books
+		assert_includes(search_posts, posts(:harry_potter), 'Harry Potter book not in search for book')
+		assert_includes(search_posts, posts(:finance_book), 'Finance textbook not in search for book')
+	end
+
 end
