2cb911344a45a5f3e5e16e26417019ab8b238a6f
zheryu <eric.zhou.us@gmail.com>
2015-11-06 17:05:52 -0800

    WORK DAMMIT

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,8 @@ class ApplicationController < ActionController::Base
   include SessionsHelper
 
   def after_sign_in_path_for(resource)
-    user_path(current_user)
+    #user_path(current_user)
+    posts_path
   end
 
   def after_sign_up_path_for(resource)
