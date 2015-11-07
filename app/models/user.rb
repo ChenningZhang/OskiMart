343df9a18a1a1fc8ebaf01f4f381e88387b25945
Chenning Zhang <czhang1306@berkeley.edu>
2015-11-06 17:51:26 -0800

    User and Post model tests passed.

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,5 +13,5 @@ class User < ActiveRecord::Base
 	validates :last_name, presence: true
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@berkeley+\.edu+\z/i
 	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
-	validates :password, presence: true
+	validates :password, presence: true, length: { minimum: 8 }
 end
