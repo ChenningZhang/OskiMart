c90238b78c41a9fde94126c91aa4abcdaec3cfba
zheryu <eric.zhou.us@gmail.com>
2015-11-06 16:29:23 -0800

    everything is awesome

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,10 @@ class ApplicationController < ActionController::Base
     user_path(current_user)
   end
 
+  def after_sign_up_path_for(resource)
+    root_path
+  end
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
