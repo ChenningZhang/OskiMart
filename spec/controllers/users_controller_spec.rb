5d5d7324b0ca84886722c9b2b411955c22154b48
Chenning Zhang <czhang1306@berkeley.edu>
2015-11-19 00:40:45 -0800

    user can see any valid user's profile; invalid ids raise errors.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -13,6 +13,16 @@ RSpec.describe UsersController, :type => :controller do
 			response.should render_template :show
 		end
 		# Should be able to show other users' profile as well
+
+		it 'renders the user profile page for any existing users' do
+			another_user = FactoryGirl.create(:user, email: "test.user@berkeley.edu")
+			get :show, id: another_user.id
+			response.should render_template :show
+		end
+
+		it 'raises error when the id given is invalid' do
+			expect{get :show, id: -1}.to raise_error(ActiveRecord::RecordNotFound)
+		end
 	end
 
 end
