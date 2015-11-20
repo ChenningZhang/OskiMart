e0856831811b6c8f573eb353a06fb3f51143c8a1
Lucas Zhang <lucas.zhang15@gmail.com>
2015-11-19 20:01:03 -0800

    Reverted back to old color scheme and added and about route

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :users
   resources :posts
   resources :comments
-
+  get '/about', :to => redirect('/about.html')
   get 'post_new' => 'posts#new'
   
   get 'posts' => 'posts#index'
