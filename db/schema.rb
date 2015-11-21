ae047c08373b80c436c75bab2acb1f3a5e7a2cba
Chenning Zhang <czhang1306@berkeley.edu>
2015-11-21 13:43:44 -0800

    Deleted database username ans password

--- db/schema.rb
+++ db/schema.rb
@@ -94,11 +94,6 @@ ActiveRecord::Schema.define(version: 20151119060345) do
     t.integer  "user_id"
   end
 
-  create_table "posts_users", id: false, force: :cascade do |t|
-    t.integer "post_id"
-    t.integer "user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
