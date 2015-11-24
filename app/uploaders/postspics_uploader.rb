1376a26eb0c30e23beb791aa731c5f12440450e4
Chenning Zhang <czhang1306@berkeley.edu>
2015-11-24 10:47:17 -0800

    Images are now replaced by no-image or user uploaded pic

--- app/uploaders/postspics_uploader.rb
+++ app/uploaders/postspics_uploader.rb
@@ -17,12 +17,12 @@ class PostspicsUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  def default_url
+    # For Rails 3.1+ asset pipeline compatibility:
+    # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+  
+    "no-image.jpg"
+  end
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
@@ -33,7 +33,7 @@ class PostspicsUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process resize_to_limit: [200, 200]
+    process resize_to_limit: [420, 400]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
