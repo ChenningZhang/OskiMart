45674d9d8959eaf7fab4aaf9d39a2f2db36a1b5e
Chenning Zhang <czhang1306@berkeley.edu>
2015-11-24 16:24:19 -0800

    Model tests cleared.

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	it 'creates valid review' do
+		@user = FactoryGirl.create(:user)
+		expect(FactoryGirl.build(:review, user_id: @user.id)).to be_valid
+	end
+
+	it 'fails to create review when feedback is blank' do
+		@user = FactoryGirl.create(:user)
+		expect(FactoryGirl.build(:review, feedback: "   ", user_id: @user.id)).not_to be_valid
+	end
+
+	it 'fails to create review when the score is out of range' do
+		@user = FactoryGirl.create(:user)
+		expect(FactoryGirl.build(:review, score: 0, user_id: @user.id)).not_to be_valid
+		expect(FactoryGirl.build(:review, score: 6, user_id: @user.id)).not_to be_valid
+	end
 end
