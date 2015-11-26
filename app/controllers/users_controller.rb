40abae98d29e9f46b16347d577086df5bd58eb26
Anusha Jain <anushajain@berkeley.edu>
2015-11-25 16:50:04 -0800

    Removed user's edit method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,10 +9,6 @@ class UsersController < ApplicationController
     @reviews = @user.reviews
   end
 
-  def edit
-    @user = User.find(params[:id])
-  end
-
   def update
   	@user = User.find(params[:id])
   	if @user.update_attributes(user_params)
